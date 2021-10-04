12f5709c60f9e2e3c9ae9051b4516956c35ecaa5
Fernao Shiotsuki <fernaospalma@gmail.com>
2021-10-04 19:52:37 -0300

    Ordem das colunas e linhas

--- script.js
+++ script.js
@@ -3,18 +3,24 @@ const gameBox = document.getElementById('gameBox')
 
 let num = 1;
 
-for(let i = 1; i <= 6; i++){
+for(let i = 1; i <= 7; i++){
 
     let tableSection = document.createElement('section');
     tableSection.classList.add(i);
     gameBox.appendChild(tableSection);
     console.log(tableSection)
 
-    for(let j = 0; j < 7; j++){
-
+    for(let j = 0; j < 6; j++){
+      
        let div = document.createElement('div');
        div.id = num;
         tableSection.appendChild(div);
         num++
     }
+}
+
+function victory (player){
+
+   
+
 }
