ba0dc5f1ccd333cba7588ed595194259d312438e
Marcos <marcoveio.bpp@gmail.com>
2021-10-05 16:36:52 -0300

    Array de parametros para condição vertical

--- script.js
+++ script.js
@@ -1,83 +1,94 @@
-const body = document.getElementsByTagName('body')[0];
+const body = document.getElementsByTagName('body')[0]
 const gameBox = document.getElementById('gameBox')
-let trackPlayerOne= []
-let trackPlayerTwo= []
-let num = 1;
+let trackPlayerOne = []
+let trackPlayerTwo = []
+let num = 1
 
-for(let i = 1; i <= 7; i++){
-   
-    let tableSection = document.createElement('section');
-    tableSection.classList.add(i);
-    gameBox.appendChild(tableSection);
-    console.log(tableSection)
+for (let i = 1; i <= 7; i++) {
+  let tableSection = document.createElement('section')
+  tableSection.classList.add(i)
+  gameBox.appendChild(tableSection)
+  console.log(tableSection)
 
-    for(let j = 0; j < 6; j++){
-      
-       let div = document.createElement('div');
-       div.setAttribute('data-square',num);
-       num++
-        tableSection.appendChild(div);
-       
-    }
-} 
-const columns = document.querySelectorAll('section');
+  for (let j = 0; j < 6; j++) {
+    let div = document.createElement('div')
+    div.setAttribute('data-square', num)
+    num++
+    tableSection.appendChild(div)
+  }
+}
+const columns = document.querySelectorAll('section')
 
-let playerOne = document.createElement('div');
-playerOne.classList.add('playerOne');
-let playerTwo = document.createElement('div');
-playerTwo.classList.add('playerTwo');   
+let playerOne = document.createElement('div')
+playerOne.classList.add('playerOne')
+let playerTwo = document.createElement('div')
+playerTwo.classList.add('playerTwo')
 
 let players = []
-let count = 0;
+let count = 0
 
-for(let i = 0 ; i <6;i++){
-    trackPlayerOne[i] = new Array(7)
-    trackPlayerTwo[i] = new Array(7)
-    
+for (let i = 0; i < 6; i++) {
+  trackPlayerOne[i] = new Array(7)
+  trackPlayerTwo[i] = new Array(7)
 }
 
-function addDiscs(evt){
+function addDiscs(evt) {
+  columns.forEach(section =>
+    section.addEventListener('click', function (evt) {
+      let columnChild = evt.currentTarget.childNodes
+      for (let i = 0; i < 6; i++) {
+        if (columnChild[i].childElementCount < 1) {
+          playerOne = document.createElement('div')
+          playerOne.classList.add('playerOne')
+          playerTwo = document.createElement('div')
+          playerTwo.classList.add('playerTwo')
 
-  columns.forEach(section=>section.addEventListener('click',function(evt){
-  
-        let columnChild = evt.currentTarget.childNodes;
-        for(let i = 0 ; i<6;i++){
-            if(columnChild[i].childElementCount<1){
+          players[0] = playerOne
+          players[1] = playerTwo
 
-                playerOne = document.createElement('div');
-                playerOne.classList.add('playerOne');
-                playerTwo = document.createElement('div');
-                playerTwo.classList.add('playerTwo');
+          let indexX = Number(columnChild[i].id)
+          let indexY = Number(section.getAttribute('class'))
 
-                players[0] = playerOne;
-                players[1] = playerTwo;
-
-                let indexX = Number(columnChild[i].id)
-                let indexY = Number(section.getAttribute('class'))
-
-                if(count===0){
-                   count=1;
-                   trackPlayerOne[-indexX+5][indexY-1] = players[0]
-                  console.log(trackPlayerOne)
-                   return  columnChild[i].appendChild(players[0]);
-                    
-
-                }else{
-                   count = 0;
-                   trackPlayerTwo[-indexX+5][indexY-1] = players[1]
-                   console.log(trackPlayerTwo)
-                   return  columnChild[i].appendChild(players[1]);
-                    
-                }
-            }
-       }  
-     
-}))
+          if (count === 0) {
+            count = 1
+            trackPlayerOne[-indexX + 5][indexY - 1] = players[0]
+            console.log(trackPlayerOne)
+            return columnChild[i].appendChild(players[0])
+          } else {
+            count = 0
+            trackPlayerTwo[-indexX + 5][indexY - 1] = players[1]
+            console.log(trackPlayerTwo)
+            return columnChild[i].appendChild(players[1])
+          }
+        }
+      }
+    })
+  )
 }
-addDiscs();
+addDiscs()
 
+function victoryDiagonal() {}
 
+//VICTORY PARAMETERS Vertical
+const winVert = [1, 2, 3, 4]
+const winningV = []
 
-function victoryDiagonal(){
-    
+function winConVertical(valor) {
+  for (i = 0; i <= 3; i++) {
+    winningV.push(valor + i)
+  }
+  winningV.push('-')
 }
+
+function createParameters() {
+  for (let i = 0; i < 6; i++) {
+    winVert.forEach(winConVertical)
+    for (let j = 0; j < winVert.length; j++) {
+      winVert[j] += 7
+    }
+  }
+}
+createParameters()
+winningV.pop()
+let winS = winningV.join(' ')
+let winArrVert = winS.split(' - ')
